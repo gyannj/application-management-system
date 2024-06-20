565d6bab5c42c5d546c9a49b0b99c76d06d6546a
gyannj <gyanjyotidas677@gmail.com>
2024-06-20 16:00:28 +0530

    Name field removed from add form

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -3,27 +3,24 @@ import { columns } from "./columns"
 import { DataTable } from "@/components/mycomponents/data-table"
 import { Application, IApplicationDocument } from "@/mongodb/models/application";
 import { currentUser } from '@clerk/nextjs/server'
-// import { useUser } from "@clerk/clerk-react";
+import { checkRole } from "@/utils/roles";
 import mongoose, {Schema,  Document, models, Model} from "mongoose";
 
 export default async function Page() {
   await connectDB();
-  // const user = await currentUser();
-  // console.log(user);
-  // const { isLoaded, isSignedIn, user } = useUser();
+ 
   const user = await currentUser();
   const userman = user?.id.toString() || ""; 
   // console.log(userman);
 
   // const data = await Application.findApplicationById(userman);
-  const data = await Application.findApplicationsByUserId(userman);
-  // const data = await Application.getAllApplications();
-    // console.log(data)
+  const data= checkRole("admin")?await Application.getAllApplications():  await Application.findApplicationsByUserId(userman);
+
   return (
     <section className="py-24">
         <div className="container">
            <h1 className="mb-6 text-3xl font-bold">All Applications</h1>
-            <DataTable columns={columns} data={data} />
+            <DataTable columns={columns} data={JSON.parse(JSON.stringify(data))} />
         </div>
 
     </section>
